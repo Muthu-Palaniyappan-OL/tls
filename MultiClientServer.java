7cd2fdf9e4a2c84fbcaf40612ed98bb3de39fc96
Muthu Palaniyappan OL <muthu892542@gmail.com>
2022-12-01 06:51:33 +0530

    Real MultiClientServer

--- MultiClientServer.java
+++ MultiClientServer.java
@@ -3,31 +3,67 @@ import java.nio.*;
 import java.nio.channels.*;
 
 public class MultiClientServer {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) throws Exception {
         var selector = Selector.open();
         var serverSocket = ServerSocketChannel.open();
-        serverSocket.bind(new InetSocketAddress("localhost", 8080));
         serverSocket.configureBlocking(false);
+        serverSocket.bind(new InetSocketAddress("localhost", PORT));
         serverSocket.register(selector, SelectionKey.OP_ACCEPT);
         var buf = ByteBuffer.allocate(1024);
         while (true) {
             selector.select();
-            var selectedKeys = selector.selectedKeys();
-            for (var i : selectedKeys) {
-                if (i.isAcceptable()) {
+            var selectedKeys = selector.selectedKeys().iterator();
+            while (selectedKeys.hasNext()) {
+                var key = (SelectionKey) selectedKeys.next();
+                selectedKeys.remove();
+
+                // New Client
+                if (key.isAcceptable()) {
                     var client = serverSocket.accept();
+                    if (client == null)
+                        continue;
                     client.configureBlocking(false);
                     client.register(selector, SelectionKey.OP_READ);
+                    System.out.println("New Connection: " + client);
                 }
-                if (i.isReadable()) {
-                    var client = (SocketChannel) i.channel();
-                    client.read(buf);
 
-                    // Main Code
+                // Old Client Reading Data
+                if (key.isReadable()) {
+                    var client = (SocketChannel) key.channel();
+                    if (client == null)
+                        continue;
 
-                    System.out.println(buf);
+                    try {
+                        buf.clear();
+                        var readBytes = client.read(buf);
+                        if (readBytes == -1) {
+                            System.out.println("Data Over");
+                            continue;
+                        }
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    } catch (Exception e) {
+                        client.close();
+                    }
                 }
-                selectedKeys.remove(i);
+
+                try {
+                    if (key.isWritable()) {
+                        var client = (SocketChannel) key.channel();
+                        if (client == null)
+                            continue;
+                        try {
+                            client.write(ByteBuffer.wrap("Muthu\n".getBytes()));
+                            key.interestOps(SelectionKey.OP_READ);
+                        } catch (Exception e) {
+                            client.close();
+                        }
+                    }
+                } catch (Exception e) {
+                    key.cancel();
+                }
+
             }
         }
     }
